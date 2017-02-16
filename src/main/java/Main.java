4b32ae2fecddd98103268875cbbedc6dce501326
Jason Skipper <jason.skipper@theironyard.com>
2017-02-16 11:17:41 -0500

    Test auto deploy...

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -19,7 +19,7 @@ public class Main {
     port(Integer.valueOf(System.getenv("PORT")));
     staticFileLocation("/public");
 
-    get("/hello", (req, res) -> "Hello World");
+    get("/hello", (req, res) -> "Hello World, We've Changed!");
 
     get("/", (request, response) -> {
         Map<String, Object> attributes = new HashMap<>();
